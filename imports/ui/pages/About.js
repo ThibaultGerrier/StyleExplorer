0abddc46f53fb2ec8ee11b86c0fa52bf19f8024e
Michael Tschuggnall <michael.tschuggnall@uibk.ac.at>
2018-07-10 15:31:33 +0200

    added DBIS and UIBK logo, adapted About site

--- imports/ui/pages/About.js
+++ imports/ui/pages/About.js
@@ -7,7 +7,8 @@ export default class About extends React.Component {
       <div>
         <h3>StyleExplorer</h3>
         <p>
-          This project is part of the bachelor thesis &quot;Visualization of Writing Styles&quot; by Thibault Gerrier (thibault.gerrier@student.uibk.ac.at) under the supervision of Michael Tschuggnall, PhD (michael.tschuggnall@uibk.ac.at)
+          This project is the result of the bachelor thesis &quot;Visualization of Writing Styles&quot; by Thibault Gerrier (thibault.gerrier@student.uibk.ac.at), conducted at the Databases and Information Systems group at the Universität Innsbruck, under the supervision of Michael Tschuggnall, PhD (michael.tschuggnall@uibk.ac.at) and Prof. Günther Specht.</p>
+          <p>
           The code can be found on github: <a href ='https://github.com/ThibaultGerrier/StyleExplorer'> link </a>
         </p>
       </div>
--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -6,8 +6,13 @@ const Index = () => (
     <Jumbotron className="text-center">
       <h2>StyleExplorer</h2>
       <p>Welcome to StyleExplorer, a tool where you can visualize your texts</p>
-      <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v0.0.1</p>
+      <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v0.0.1</p>		
     </Jumbotron>
+    <div className="text-center" style={{marginTop:'50px'}}>      
+      <p>powered by</p>
+      <div><a target="_blank" href="https://dbis-informatik.uibk.ac.at"><img src='dbis-logo.png' height="100"/></a></div>
+      <div><a target="_blank" href="https://www.uibk.ac.at"><img src='uibk-logo.png' height="100"/></a></div>
+    </div>
   </div>
 );
 
